0fd37732a167740625b2be7d40c7b15b91d2b684
Aysel <hagaysel@gmail.com>
2020-11-22 19:19:47 -0500

    deneme 2 Aysel,sWay baslangic

--- src/GitDen1/GitHub.java
+++ src/GitDen1/GitHub.java
@@ -3,6 +3,7 @@ package GitDen1;
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Big Dream");
 
     }
 }
