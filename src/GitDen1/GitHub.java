5617f97ac13ae6981ccf1445990fa9abc88a5d8a
Aysel <hagaysel@gmail.com>
2020-11-22 19:21:19 -0500

    deneme 2 Aysel,sWay devami

--- src/GitDen1/GitHub.java
+++ src/GitDen1/GitHub.java
@@ -4,6 +4,7 @@ public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Big Dream");
+        System.out.println("Smile");
 
     }
 }
